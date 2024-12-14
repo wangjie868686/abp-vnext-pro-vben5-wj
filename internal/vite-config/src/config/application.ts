4a1f6aad768e38954d1f54e33b3db9aaffcfd3ed
Hanpaopao <510423039@qq.com>
2024-12-14 17:09:30 +0800

    feat: 升级vben5.5.1

--- internal/vite-config/src/config/application.ts
+++ internal/vite-config/src/config/application.ts
@@ -44,7 +44,7 @@ function defineApplicationConfig(userConfigPromise?: DefineApplicationOptions) {
       nitroMockOptions: {},
       print: !isBuild,
       printInfoMap: {
-        'Vben Admin Docs': 'https://doc.vben.pro',
+        'Vben Admin Docs': 'https://doc.cncore.club/',
       },
       pwa: true,
       pwaOptions: getDefaultPwaOptions(appTitle),
